03173f5f77e9fe110ea4ba32cb1bada0868926df
Brian Kibet <kibetsbrian@gmail.com>
2023-10-07 10:01:53 +0300

    add product model

--- ft_backend2/models.py
+++ ft_backend2/models.py
@@ -14,4 +14,15 @@ class User(db.Model):
     products = db.relationship('Product', backref='user', lazy=True)
 
 
+class Product(db.Model):
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(255), nullable=False)
+    product_image = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    size = db.Column(db.String(50))
+    category = db.Column(db.String(255))
+    price = db.Column(db.Float)
+    in_stock = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
