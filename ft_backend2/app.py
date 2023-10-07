0e63f38aa9d05a963d5c039ec79edcbed94a1597
Brian Kibet <kibetsbrian@gmail.com>
2023-10-07 10:05:34 +0300

    add login route

--- ft_backend2/app.py
+++ ft_backend2/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from flask_restful import Api, Resource, reqparse
+import bcrypt
 
 app = Flask(__name__)
 
@@ -20,20 +21,16 @@ class UserRegistrationResource(Resource):
     def post(self):
         data = request.get_json()
 
-        # Check if username or email already exists
         existing_user = User.query.filter((User.username == data['username']) | (User.email == data['email'])).first()
         if existing_user:
             return {'message': 'Username or email already exists'}, 400
 
-        # Hash the password
         hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
 
-        # Create a new user
         new_user = User(
             username=data['username'],
             email=data['email'],
             password_hash=hashed_password.decode('utf-8'),
-            # Add other user attributes here
         )
 
         db.session.add(new_user)
@@ -43,3 +40,18 @@ class UserRegistrationResource(Resource):
 
 
 api.add_resource(UserRegistrationResource, '/register')
+
+class UserLoginResource(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+
+        if user and bcrypt.checkpw(data['password'].encode('utf-8'), user.password_hash.encode('utf-8')):
+            
+            access_token = create_access_token(identity=user.user_id)
+            return {'access_token': access_token}, 200
+        else:
+            return {'message': 'Invalid credentials'}, 401
+
+
+api.add_resource(UserLoginResource, '/login')
