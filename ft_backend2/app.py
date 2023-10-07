ad5a98cde2a8bd5bd0de88f3989f2dc049e76bb4
Brian Kibet <kibetsbrian@gmail.com>
2023-10-07 09:59:29 +0300

    add imports

---
+++ ft_backend2/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Threadup.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+api = Api(app)
