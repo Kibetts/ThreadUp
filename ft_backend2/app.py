e163652600506d5ac17e9c99eb60ca35230f2a4d
Brian Kibet <kibetsbrian@gmail.com>
2023-10-07 10:03:42 +0300

    add registration route

--- ft_backend2/app.py
+++ ft_backend2/app.py
@@ -13,3 +13,33 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 api = Api(app)
+
+from models import User, Product 
+
+class UserRegistrationResource(Resource):
+    def post(self):
+        data = request.get_json()
+
+        # Check if username or email already exists
+        existing_user = User.query.filter((User.username == data['username']) | (User.email == data['email'])).first()
+        if existing_user:
+            return {'message': 'Username or email already exists'}, 400
+
+        # Hash the password
+        hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+
+        # Create a new user
+        new_user = User(
+            username=data['username'],
+            email=data['email'],
+            password_hash=hashed_password.decode('utf-8'),
+            # Add other user attributes here
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return {'message': 'User registered successfully'}, 201
+
+
+api.add_resource(UserRegistrationResource, '/register')
